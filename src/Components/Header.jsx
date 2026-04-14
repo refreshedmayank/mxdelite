f7ee8da56795665de1f69bc93a79f09428e3c401
refreshedmayank <refreshedmayank@gmail.com>
2026-04-14 10:03:24 +0530

    Header structure is created

---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+// Creating Header Function
+function Header(){
+  return (
+    // Creating container
+    <div>
+      <h1>Mxdelite</h1>
+       {/* Creating Nav container */}
+      <nav>
+        {/* Nav elements */}
+        <a href="">Services</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+        <a href="">Start Your Project</a>
+      </nav>
+    </div>
+  );
+}
+export default Header; // exporting to be imported in future
