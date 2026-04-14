57f3ea1b01889ad89155d3d8b5e530137597e596
refreshedmayank <refreshedmayank@gmail.com>
2026-04-14 12:20:42 +0530

    adding styles

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,7 @@ function Hero(){
   return (
     <div>
       <h1>We turn invisible businesses into brands that own the room.</h1>
-      <p className="text-center  text-red-500 ">Websites</p>
+      <p className="">Websites</p>
       <p>Brand Identity</p>
       <p>Social Media</p>
       <button>START YOUR PROJECT</button>
