f4bc5a13a82d65bb589ddb1ab31460098c37a1fe
refreshedmayank <refreshedmayank@gmail.com>
2026-04-14 10:16:39 +0530

    Created Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Components/Header'
+import Hero from './Components/Hero'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Hero / >
     </div>
   )
 }
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,14 @@
+// Creating Hero funtion
+function Hero(){
+  return (
+    <div>
+      <h1>We turn invisible businesses into brands that own the room.</h1>
+      <p>Websites</p>
+      <p>Brand Identity</p>
+      <p>Social Media</p>
+      <button>START YOUR PROJECT</button>
+    </div>
+
+  );
+}
+export default Hero; // Exporting to be imported in future
