be1358b39c727708f2c83ef1fc40bbbd61aede55
refreshedmayank <refreshedmayank@gmail.com>
2026-04-15 12:48:18 +0530

    completed problems structure and basic styling

--- src/Components/Problems.jsx
+++ src/Components/Problems.jsx
@@ -1,29 +1,34 @@
 function Problems(){
   return (
     <div className="grid-container grid grid-cols-2 gap-10 p-10">
-      <div className="Left-side-column  border">
+      <div className="Left-side-column">
         <span>01 — The Problem</span>
-        <h2>Your product is good. Your brand is why people aren't buying.</h2>
-        <p>
-          Most businesses lose customers before they even get a chance to sell.
-          The product is not the problem. The first impression is. We fix that.
-        </p>
+        <h2 className="flex flex-col gap-2 items-center">
+          <span>Your product is good.</span>
+          <span>Your brand is why</span>
+          <span>people aren't</span>
+          <span>buying.</span>
+          </h2>
+
+
+         <p>Most businesses lose customers before they even get a chance to sell.
+        The product is not the problem. The first impression is. We fix that.</p>
       </div>
       <div className="Righ-column-divs flex flex-col gap-6 ">
-        <div className="Startup border">
+        <div className="Startup border p-10">
           <span>Startup</span>
           <h3>
             Have the idea. Don't have the presentation. That's the gap we fill.
           </h3>
         </div>
-        <div className="Ecommerce border">
+        <div className="Ecommerce border p-10">
           <span>Ecommerce</span>
           <h3>
             Spending on ads but losing sales at first impression. Branding is
             the fix they are not pulling
           </h3>
         </div>
-        <div className="Business border">
+        <div className="Business border p-10 ">
           <span>Business</span>
           <h3>
             Your online presence is your first impression. We make sure it is
