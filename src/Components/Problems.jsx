9a8bf66d12f89bc6c80660610875f0e8956f89d2
refreshedmayank <refreshedmayank@gmail.com>
2026-04-15 12:22:07 +0530

    fixed issues reagarding problems

--- src/Components/Problems.jsx
+++ src/Components/Problems.jsx
@@ -1,6 +1,6 @@
 function Problems(){
   return (
-    <div>
+    <div className="grid-container grid">
       <div className="Left-side-column">
         <span>01 — The Problem</span>
         <h2>Your product is good. Your brand is why people aren't buying.</h2>
@@ -11,7 +11,7 @@ function Problems(){
       </div>
       <div className="Righ-column-divs">
         <div className="Startup">
-          <Span>Startup</Span>
+          <span>Startup</span>
           <h3>
             Have the idea. Don't have the presentation. That's the gap we fill.
           </h3>
@@ -20,14 +20,14 @@ function Problems(){
           <span>Ecommerce</span>
           <h3>
             Spending on ads but losing sales at first impression. Branding is
-            the // fix they are not pulling
+            the fix they are not pulling
           </h3>
         </div>
         <div className="Business">
           <span>Business</span>
           <h3>
             Your online presence is your first impression. We make sure it is
-            the // right one
+            the right one
           </h3>
         </div>
       </div>
