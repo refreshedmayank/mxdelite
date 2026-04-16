8420b6e0242fab841a40eccaed2af145fb0f861b
refreshedmayank <refreshedmayank@gmail.com>
2026-04-16 17:08:25 +0530

    centered input labels

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -19,11 +19,11 @@ function Contact() {
       </div>
 
       <div className=" p-6 flex flex-col text-lg">
-        <label htmlFor="name">Name</label>
+        <label className="text-center" htmlFor="name">Name</label>
         <input  className="rounded-sm bg-black border" type="text" id="name" />
-        <label htmlFor="email">Email</label>
+        <label className="text-center" htmlFor="email  ">Email</label>
         <input className="rounded-sm border bg-transparent text-white " type="email" id="email" />
-        <label htmlFor="project-brief">Project Brief</label>
+        <label className="text-center" htmlFor="project-brief">Project Brief</label>
         <textarea className="rounded-sm bg-transparent border text-white " id="project-brief" name="project-brief"></textarea>
         <button className=  " rounded-sm mt-5 bg-transparent border text-white p-2 w-40 m-auto" type="submit">Submit</button>
       </div>
