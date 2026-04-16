c790433e1a8e5a394da12602f2a7f1125f4e3233
refreshedmayank <refreshedmayank@gmail.com>
2026-04-16 10:52:35 +0530

    add mobile header with hamburger toggle

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,29 +3,31 @@ function Header() {
   const [isopen, setisopen] = useState(false)
   return (
     <div>
-      <div className="border text-center">
-        <span>Mxdelite</span>
-        <button onClick={() => setisopen(!isopen)}>
+      <div className="border text-center flex justify-between items-center p-4">
+        <button className="w-10" onClick={() => setisopen(!isopen)}>
           {isopen ? "x" : "☰"}
         </button>
+        <span>Mxdelite</span>
+        <div className="w-10"></div>
       </div>
       {isopen && (
-      <nav>
-        <ul>
-          <li>
-            <a href="#Services">Services</a>
-          </li>
-          <li>
-            <a href="#About">About</a>
-          </li>
-          <li>
-            <a href="#Contact">Contact</a>
-          </li>
-          <li>
-            <a href="#Start Your Project">Start Your Project</a>
-          </li>
-        </ul>
-      </nav>)}
+        <nav>
+          <ul>
+            <li>
+              <a href="#Services">Services</a>
+            </li>
+            <li>
+              <a href="#About">About</a>
+            </li>
+            <li>
+              <a href="#Contact">Contact</a>
+            </li>
+            <li>
+              <a href="#Start Your Project">Start Your Project</a>
+            </li>
+          </ul>
+        </nav>
+      )}
     </div>
   );
 }
