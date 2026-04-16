8117e2d234966aab71fdbe155914b4a2a4a48ffe
refreshedmayank <refreshedmayank@gmail.com>
2026-04-16 17:06:18 +0530

    fixed styling on inputs

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -20,12 +20,12 @@ function Contact() {
 
       <div className=" p-6 flex flex-col text-lg">
         <label htmlFor="name">Name</label>
-        <input className="" type="text" id="name" />
+        <input  className="rounded-sm bg-black border" type="text" id="name" />
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
+        <input className="rounded-sm border bg-transparent text-white " type="email" id="email" />
         <label htmlFor="project-brief">Project Brief</label>
-        <textarea id="project-brief" name="project-brief"></textarea>
-        <button className="mt-5 bg-white text-black p-2 w-40 m-auto" type="submit">Submit</button>
+        <textarea className="rounded-sm bg-transparent border text-white " id="project-brief" name="project-brief"></textarea>
+        <button className=  " rounded-sm mt-5 bg-transparent border text-white p-2 w-40 m-auto" type="submit">Submit</button>
       </div>
     </div>
   );
