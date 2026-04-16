d8d48176b15bf402fbea1588b351be6bbe69469e
refreshedmayank <refreshedmayank@gmail.com>
2026-04-16 13:10:11 +0530

    styling hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,16 +1,14 @@
 function Hero() {
   return (
-    <div>
-      <section>
-        <div>
-          <h1>We turn invisible businesses into brands that own the room.</h1>
-        </div>
-        <div>
+    <div className=" mt-24 p-24 min-h-screen  flex flex-col justify-center">
+      <section className="  p-3 text-center">
+          <h1 className="text-4xl font-bold ">We turn invisible businesses into brands that own the room.</h1>
+          <div className="flex items-start  text-xs  ">
           <span>Websites</span>
           <span>Brand Identity</span>
           <span>Social Media</span>
-          <a href="">START YOUR PROJECT</a>
-        </div>
+
+          </div>
       </section>
     </div>
   );
