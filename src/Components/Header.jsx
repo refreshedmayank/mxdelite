6154bfee3d7c4ebfb2a2d8e90f978e92ef13acc8
refreshedmayank <refreshedmayank@gmail.com>
2026-04-17 13:08:49 +0530

    added close menu functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 function Header() {
   const [isopen, setisopen] = useState(false)
+  const closemenu = () =>{
+    setisopen(false)}
   return (
     <div>
       <div className="border bg-black  text-center flex justify-between items-center p-4 z-50 fixed top-0 left-0 w-full ">
@@ -14,10 +16,15 @@ function Header() {
       {isopen && (
         <nav className="bg-black  fixed top-0 left-0 h-full w-full text-white">
           <div className="mt-20 p-6 flex flex-col gap-10 text-center">
-            <Link className="" to="/services">
-              Services</Link>
-            <Link to="/contact">Contacts</Link>
-            <Link to="/problems">Problems</Link>
+            <Link className="" to="/services" onClick={closemenu}>
+              Services
+            </Link>
+            <Link to="/contact" onClick={closemenu}>
+              Contacts
+            </Link>
+            <Link to="/problems" onClick={closemenu}>
+              Problems
+            </Link>
           </div>
         </nav>
       )}
