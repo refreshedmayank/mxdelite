e13ab4a76c90f0fe090079ed9c78cd0c001e1781
refreshedmayank <refreshedmayank@gmail.com>
2026-04-17 14:11:37 +0530

    added email redirection in footer

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,9 +1,17 @@
 function Footer() {
+  function sendtoinstagram(){
+
+  }
+  function sendtoemail(){
+    const Email = "mxdelite@gmail.com"
+    window.location.href = `mailto:${Email}`;
+
+  }
   return (
     <div className="p-4 ">
     <div className="      bg-black flex  p-4 gap-4 justify-center items-center text-lg">
-      <h2>Instagram</h2>
-      <h2>Email</h2>
+      <h2 onClick={sendtoinstagram} >Instagram</h2>
+      <h2 onClick={sendtoemail}>Email</h2>
     </div>
     </div>
   );
