91803f610487df09bda5e43e94d0f5bca3b12595
refreshedmayank <refreshedmayank@gmail.com>
2026-04-17 14:28:34 +0530

    added white borders in cards in problem endpoint

--- src/Components/Problems.jsx
+++ src/Components/Problems.jsx
@@ -1,7 +1,7 @@
 function Problems() {
   return (
     <div>
-      <div className="flex flex-col justify-center items-center p-5 bg-black text-xl text-center  gap-">
+      <div className="mt-12 flex flex-col justify-center items-center p-5 bg-black text-xl text-center  gap-">
         <span className="">01 — The Problem</span>
         <h2>Your product is good. Your brand is why people aren't buying.</h2>
         <p>
@@ -10,20 +10,20 @@ function Problems() {
         </p>
       </div>
       <div className="flex flex-col gap-6 text-lg ">
-        <div className="border border-b-white/20 p-3 mt-5">
+        <div className="border rounded-md p-3 mt-5">
           <span>Startup</span>
           <h3>
             Have the idea. Don't have the presentation. That's the gap we fill.
           </h3>
         </div>
-        <div className="border border-b-white/20 p-3">
+        <div className="border rounded-md p-3">
           <span>Ecommerce</span>
           <h3>
             Spending on ads but losing sales at first impression. Branding is
             the fix they are not pulling.
           </h3>
         </div>
-        <div className="border border-bwhite/20 p-3">
+        <div className="border rounded-md border-bwhite/20 p-3">
           <span>Business</span>
           <h3>
             Your online presence is your first impression. We make sure it is
