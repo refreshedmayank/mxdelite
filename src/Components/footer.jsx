e709eb0828639ca5fc3decc004e60ff83524d014
refreshedmayank <refreshedmayank@gmail.com>
2026-04-20 10:33:45 +0530

    made footer component

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
   }
   return (
     <div className="p-4 ">
-    <div className="      bg-black flex  p-4 gap-4 justify-center items-center text-lg">
+    <div className="footer">
       <h2 onClick={sendtoinstagram} >Instagram</h2>
       <h2 onClick={sendtoemail}>Email</h2>
     </div>
