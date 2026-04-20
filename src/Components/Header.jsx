877a8442cdd5780820afda9323f16b7e58b90d8d
refreshedmayank <refreshedmayank@gmail.com>
2026-04-20 12:22:10 +0530

    fixed footer to bottom with full width

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
     
     <div>
       <div className="header">
-        <button className="w-10"  onClick={() => setisopen(!isopen)}>
+        <button className="w-10 menu-btn" onClick={() => setisopen(!isopen)}>
           {isopen ? "x" : "☰"}
         </button>
         <span className="font-['Montserrat']">Mxdelite</span>
@@ -16,17 +16,19 @@ function Header() {
       </div>
       {isopen && (
         <nav className="menu">
-          <div className="mt-20 p-6 flex flex-col gap-10 text-center">
-            <Link to="/" onClick={closemenu}>Home</Link>
-            <Link className="" to="/services" onClick={closemenu}>
-              Services
-            </Link>
-            <Link to="/contact" onClick={closemenu}>
-              Contacts
-            </Link>
-            <Link to="/problems" onClick={closemenu}>
-              Problems
-            </Link>
+          <div className="menu-bg">
+            <div className="mt-20 p-6 flex flex-col gap-10 text-center">
+              <Link to="/" onClick={closemenu}>Home</Link>
+              <Link className="" to="/services" onClick={closemenu}>
+                Services
+              </Link>
+              <Link to="/contact" onClick={closemenu}>
+                Contacts
+              </Link>
+              <Link to="/problems" onClick={closemenu}>
+                Problems
+              </Link>
+            </div>
           </div>
         </nav>
       )}
